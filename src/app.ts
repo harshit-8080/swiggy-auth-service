83f2223670810d34f6cd5286f3cc5bb8f7b0e44f
harshit-8080 <harshitraj@gytworkz.com>
2023-09-12 18:08:58 +0530

    added types

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to auth service');
 });
 export default app;
