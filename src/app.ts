390d567874c43c8b7de10097eef26008a19374ae
harshit-8080 <harshitraj@gytworkz.com>
2023-09-13 15:29:58 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { HttpError } from 'http-errors';
+import { logger } from './config/logger';
 
 const app = express();
 
@@ -9,4 +11,15 @@ app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to auth service');
 });
+
+app.use((err: HttpError, req: Request, res: Response) => {
+  logger.error(err.message);
+  res.status(err.statusCode).json({
+    name: err.name,
+    statusCode: err.statusCode,
+    message: err.message,
+    details: [],
+  });
+});
+
 export default app;
