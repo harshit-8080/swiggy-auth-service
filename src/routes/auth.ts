0a86d0260a9bbdab58f7b593207292f4c6e0f32d
harshit-8080 <harshitrajlnctcse@gmail.com>
2023-10-02 13:00:43 +0530

    added register user test

---
+++ src/routes/auth.ts
@@ -0,0 +1,12 @@
+import { AuthController } from '../controllers/AuthController';
+import express from 'express';
+
+const router = express.Router();
+
+const authController = new AuthController();
+
+router.post('/register', (req, res) => {
+  authController.register(req, res);
+});
+
+export default router;
