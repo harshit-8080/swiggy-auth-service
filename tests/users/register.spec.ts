54abdd7f396a0d195894593e993b92439adc6269
harshit-8080 <harshitrajlnctcse@gmail.com>
2023-10-02 13:09:34 +0530

    added test to validate json response

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -18,5 +18,23 @@ describe('POST /auth/register', () => {
       // Assert
       expect(response.statusCode).toBe(201);
     });
+
+    it('should return valid json response', async () => {
+      // Arrange
+      const userData = {
+        firstName: 'Harshit',
+        lastName: 'Raj',
+        email: 'harshit.new71@gmail.com',
+        password: 'secret@123',
+      };
+
+      // Act
+      const response = await request(app).post('/auth/register').send(userData);
+
+      // Assert
+      expect(response.headers['content-type']).toEqual(
+        expect.stringContaining('json'),
+      );
+    });
   });
 });
